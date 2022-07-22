fd02475d7a71c6e86c83a806d60c0a7a64093cc2
Freewind <mnpham1986@gmail.com>
2022-07-22 19:15:50 +0700

    ...

--- 10. Database/Select_Join_Union_EmployeesWithMissingInformation/Select_Join_Union_MissingInfo.sql
+++ 10. Database/Select_Join_Union_EmployeesWithMissingInformation/Select_Join_Union_MissingInfo.sql
@@ -3,7 +3,7 @@ select Employees.employee_id
 from Employees 
 left join Salaries                                      
 on Employees.employee_id=Salaries.employee_id           /* select all employee_ids from 2 tables */
-where Salaries.salary is null                           /* pick out employee_ids where salary is null */
+where Salaries.salary is null                           /* pick out employee_ids where salary is nmissing */
 
 union
 
@@ -11,6 +11,6 @@ select Salaries.employee_id
 from Salaries 
 left join Employees                                      
 on Employees.employee_id=Salaries.employee_id           /* select all employee_ids from 2 tables */
-where Employees.name is null                            /* pick out employee_ids where name is null */
+where Employees.name is null                            /* pick out employee_ids where name is missing */
 
 order by employee_id
