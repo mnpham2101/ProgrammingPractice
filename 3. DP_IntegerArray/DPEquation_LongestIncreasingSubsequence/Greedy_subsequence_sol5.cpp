9790263203a166ddfa5d9755f8b534042a809296
Freewind <mnpham1986@gmail.com>
2022-10-23 15:12:58 +0700

    ...

--- 3. DP_IntegerArray/DPEquation_LongestIncreasingSubsequence/Greedy_subsequence_sol5.cpp
+++ 3. DP_IntegerArray/DPEquation_LongestIncreasingSubsequence/Greedy_subsequence_sol5.cpp
@@ -1,16 +1,6 @@
 #include<vector>
 #include<iostream>
 using namespace std;
-/*
-Greedy solution:
-- This method provides a way to keep a best sequence while we iterative over the array
-- Starting case: The best sequence when i = 0 is nums[0]
-- If we have a current best sequence and we meet a new val:
-  (1) if val > all elements, can easily add it into the tail
-  (2) if not, can replace lower_bound of val in the sequence by val because:
-     + it doesn't change the current result and all next result (e.g 2 9 10 14 and 2 4 10 14, no matter what the next numbers are)
-     + it is always better way to find an optimal sequence, because we reduce the condition for (1) as much as possible
-*/
 
 int lengthOfLISGreedy(vector<int>& nums) {
     int n = nums.size();
